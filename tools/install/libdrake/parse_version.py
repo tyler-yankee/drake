7fbf1624aed301726cf4ea40e468e8fd09511d63
Tyler Yankee <tyler.yankee@kitware.com>
2026-08-14 08:08:40 -0400

    wip: [install] Restructure parse_version.py
    pythonify

--- tools/install/libdrake/parse_version.py
+++ tools/install/libdrake/parse_version.py
@@ -2,107 +2,131 @@
 which specifies the variable substitutions needed for drake-config.cmake."""
 
 import argparse
+from pathlib import Path
 import re
+import sys
+import textwrap
 
-VERSION_TAG = "STABLE_VERSION"
 
+class DrakeVersion:
+    VERSION_TAG = "STABLE_VERSION"
 
-# Check if a version string conforms to PEP 440.
-def _check_version(version):
-    return (
-        re.match(
-            r"^([1-9][0-9]*!)?(0|[1-9][0-9]*)"
-            r"(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?"
-            r"(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?"
-            r"([+][a-z0-9]+([-_\.][a-z0-9]+)*)?$",
-            version,
+    def __init__(self, stamp_file: Path, output_file: Path):
+        self.stamp_file = stamp_file
+        self.output_file = output_file
+        self.version_full = None
+        self.version_parts = None
+
+    @staticmethod
+    def _check_version(version: str) -> bool:
+        """Check if our version string conforms to PEP 440."""
+        return (
+            re.match(
+                r"^([1-9][0-9]*!)?(0|[1-9][0-9]*)"
+                r"(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?"
+                r"(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?"
+                r"([+][a-z0-9]+([-_\.][a-z0-9]+)*)?$",
+                version,
+            )
+            is not None
         )
-        is not None
-    )
 
+    def _parse_stamp(self) -> None:
+        """
+        Extract full version and version parts from version stamp file.
 
-# Extract full version and version parts from version stamp file.
-#
-# If a version is specified, the input file should contain a line starting with
-# 'STABLE_VERSION', which should be three space-separated words; the tag, the
-# full version, and the git SHA.
-#
-# This extracts the (full) version identifier, as well as the individual
-# numeric parts (separated by '.') of the version. Any pre-release, 'dev',
-# 'post', and/or local identifier (i.e. portion following a '+') is discarded
-# when extracting the version parts. If version information is not found,
-# this returns (None, None).
-def _parse_stamp(stamp_file):
-    # Read input.
-    for line in stamp_file:
-        if line.startswith(VERSION_TAG):
-            tag, version_full, _git_sha = line.strip().split()
-            assert tag == VERSION_TAG
+        If a version is specified, the input file should contain a line
+        starting with 'STABLE_VERSION', which should be three space-separated
+        words; the tag, the full version, and the git SHA.
 
-            # Check version format and extract numerical components.
-            if not _check_version(version_full):
-                raise ValueError(f"Version {version_full} is not valid")
-            if re.match(r"^[1-9][0-9]*!", version_full):
-                raise ValueError(
-                    f"Version {version_full} contains an epoch,"
-                    " which is not supported at this time"
-                )
+        This extracts the (full) version identifier, as well as the individual
+        numeric parts (separated by '.') of the version. Any pre-release,
+        'dev', 'post', and/or local identifier (i.e. portion following a '+')
+        is discarded when extracting the version parts. If version information
+        is not found, self.version_* attributes remain `None`.
+        """
+        # Read input.
+        stamp = self.stamp_file.read_text(encoding="utf-8")
+        for line in stamp:
+            if line.startswith(self.VERSION_TAG):
+                tag, version_full, _git_sha = line.strip().split()
+                assert tag == self.VERSION_TAG, tag
 
-            m = re.match(r"^[0-9.]+", version_full)
-            assert m
-
-            # Check for sufficient version parts (note: user and continuous
-            # builds may have more than three parts) and pad to ensure we
-            # always have four.
-            version_parts = m.group(0).split(".")
-            if len(version_parts) < 4:
-                if len(version_parts) == 3:
-                    version_parts.append(0)
-                else:
+                # Check version format and extract numerical components.
+                if not self._check_version(version_full):
+                    raise ValueError(f"Version {version_full} is not valid")
+                if re.match(r"^[1-9][0-9]*!", version_full):
                     raise ValueError(
-                        f"Version {version_full} does not have enough parts"
+                        f"Version {version_full} contains an epoch,"
+                        " which is not supported at this time"
                     )
 
-            return version_full, tuple(map(int, version_parts))
+                m = re.match(r"^[0-9.]+", version_full)
+                assert m, version_full
 
-    return None, None
+                # Check for sufficient version parts (note: user and continuous
+                # builds may have more than three parts) and pad to ensure we
+                # always have four.
+                version_parts = m.group(0).split(".")
+                if len(version_parts) < 4:
+                    if len(version_parts) == 3:
+                        version_parts.append(0)
+                    else:
+                        raise ValueError(
+                            f"Version {version_full} does not have enough parts"
+                        )
+
+                self.version_full = version_full
+                self.version_parts = tuple(map(int, version_parts))
+
+    def _write_version_info(self) -> None:
+        """Write version information to CMake cache-style script."""
+        if self.version_full is None:
+            # The full version is reported as "unknown", but the numeric
+            # components are reported as 0 so that they remain usable as
+            # integers, e.g. in the DRAKE_VERSION_* preprocessor macros in
+            # drake/version.h.
+            self.output_file.write_text(
+                textwrap.dedent("""
+                set(DRAKE_VERSION "unknown")
+                set(DRAKE_VERSION_MAJOR "0")
+                set(DRAKE_VERSION_MINOR "0")
+                set(DRAKE_VERSION_PATCH "0")
+                set(DRAKE_VERSION_TWEAK "0")
+            """)
+            )
+        else:
+            self.output_file.write_text(
+                textwrap.dedent(f"""
+                set(DRAKE_VERSION "{self.version_full}")
+                set(DRAKE_VERSION_MAJOR "{self.version_parts[0]}")
+                set(DRAKE_VERSION_MINOR "{self.version_parts[1]}")
+                set(DRAKE_VERSION_PATCH "{self.version_parts[2]}")
+                set(DRAKE_VERSION_TWEAK "{self.version_parts[3]}")
+            """)
+            )
+
+    def parse_version(self) -> None:
+        """Parse the version stamp file and write to output."""
+        self._parse_stamp()
+        self._write_version_info()
 
 
-# Write version information to CMake cache-style script.
-def _write_version_info(out, version_full, version_parts):
-    if version_full is None:
-        # The full version is reported as "unknown", but the numeric components
-        # are reported as 0 so that they remain usable as integers, e.g. in the
-        # DRAKE_VERSION_* preprocessor macros in drake/version.h.
-        out.write('set(DRAKE_VERSION "unknown")\n')
-        out.write('set(DRAKE_VERSION_MAJOR "0")\n')
-        out.write('set(DRAKE_VERSION_MINOR "0")\n')
-        out.write('set(DRAKE_VERSION_PATCH "0")\n')
-        out.write('set(DRAKE_VERSION_TWEAK "0")\n')
-    else:
-        out.write(f'set(DRAKE_VERSION "{version_full}")\n')
-        out.write(f'set(DRAKE_VERSION_MAJOR "{version_parts[0]}")\n')
-        out.write(f'set(DRAKE_VERSION_MINOR "{version_parts[1]}")\n')
-        out.write(f'set(DRAKE_VERSION_PATCH "{version_parts[2]}")\n')
-        out.write(f'set(DRAKE_VERSION_TWEAK "{version_parts[3]}")\n')
-
-
-def main():
+def main(argv=None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "input",
-        type=argparse.FileType("r"),
+        type=Path,
         help="Path to file optionally containing stamp version.",
     )
-    parser.add_argument(
-        "output", type=argparse.FileType("w"), help="Path to output file."
-    )
-    args = parser.parse_args()
+    parser.add_argument("output", type=Path, help="Path to output file.")
+    args = parser.parse_args(argv)
 
-    _write_version_info(args.output, *_parse_stamp(args.input))
+    version = DrakeVersion(args.input, args.output)
+    version.parse_version()
 
     return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(sys.argv[1:]))
